43246f2bd8b9ae2ac7e98fa7185150527b72f880
Hieu Trinh <hieu.trinh@tomra.com>
2023-11-30 22:14:40 +1300

    As diabete package
    With trainDiabete and inteferenceDiabete as CLI

---
+++ diabete/inferenceDiabete.py
@@ -0,0 +1,18 @@
+
+import argparse
+
+from . import diabete
+import pandas as pd
+
+def main():
+    parser = argparse.ArgumentParser("Run inference (aka prediction) from trained model")
+    parser.add_argument("model",help="Path to trained model.")
+    parser.add_argument("input",help="Path to the input csv to run prediction against. The column order must be the same as the one used to train the model (and excluding the last target column)")
+
+    args = parser.parse_args()
+
+
+    diabete.inference(args.input,args.model)
+
+if __name__ == "__main__":
+    main()
---
+++ diabete/trainDiabete.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import argparse
+
+from . import diabete
+
+def main():
+    parser = argparse.ArgumentParser("Train model using csv")
+    parser.add_argument("data",help="Csv file used to train the model. The last column is used as target.")
+    parser.add_argument("output",help="Path to file to witch the model will be saved.")
+    parser.add_argument("--testSize",default=0.1, help="Ratio of data to be used as split. Default: %(default)f")
+    parser.add_argument("-q","--quiet",default=False, help="Quiet mode. Do not show stats. Default: False")
+    parser.add_argument("--layer",default="(10,10,10)", help="Define the sizes of hidden layers of the MLP. Default: %(default)s")
+    args = parser.parse_args()
+    diabete.train(csv_path=args.data,
+                  trained_model_path=args.output,
+                  test_size=args.testSize,
+                  verbose=not args.quiet,
+                  hidden_layer=eval(args.layer))
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
